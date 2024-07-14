982ffe4f3c78323fb48fc6ef0cb1aff3324e730c
Yux77 <forbidbanoutlook@gmail.com>
2024-07-15 03:32:19 +0800

    home start

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -3,7 +3,30 @@ import './home.styles.scss';
 const Home = () => {
     return (
         <>
-            <div>HOME</div>
+            <div className="homeBox">
+                <div className="cardFlow">
+                    <div className="articleCardMedium"></div>
+                    <div className="articleCardSmall"></div>
+                    <div className="articleCardMedium"></div>
+                </div>
+                <div className="cardFlow">
+                    <div className="articleCardLarg"></div>
+                    <div className="articleCardSmall"></div>
+                </div>
+                <div className="cardFlow">
+                    <div className="articleCardSmall"></div>
+                    <div className="articleCardMedium"></div>
+                    <div className="articleCardMedium"></div>
+                </div>
+                <div className="cardFlow">
+                    <div className="articleCardLarg"></div>
+                    <div className="articleCardLarg"></div>
+                </div>
+                <div className="cardFlow">
+                    <div className="articleCardMedium"></div>
+                    <div className="articleCardMedium"></div>
+                </div>
+            </div>
         </>
     );
 }
